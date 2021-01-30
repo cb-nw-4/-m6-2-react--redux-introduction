1a580dc54d687aa07cdfbf7790eeab11b7ad2648
Joni Sadler <joni.anne.sadler@gmail.com>
2021-01-30 13:41:10 -0500

    Exercise 3a complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-
 const initialState = {};
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
+    case 'ADD_ITEM': {
+      return {
+        ...state,
+        [action.item.id]: {
+          ...action.item,
+          quantity: 1,
+        }
+      }
+    }
     default:
       return state;
   }
