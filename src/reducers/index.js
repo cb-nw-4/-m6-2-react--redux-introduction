5e9d1bd1e4add3f5f55e1bf7563c7309fbdb6ff5
Doug Wright <dw@nof49.ca>
2021-02-01 12:03:14 -0500

    Completed exercise 3a

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,16 @@ const initialState = {};
 
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'ADD_ITEM': {
+      return {
+        ...state,
+        [action.item.id]: {
+          ...action.item,
+          quantity: 1
+        }
+      }
+    }
+    
     default:
       return state;
   }
