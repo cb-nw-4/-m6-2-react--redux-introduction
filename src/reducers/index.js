4cf7b8f730361b581d79027f282d4f1b72451d7a
chris shino <chris.shino@hotmail.com>
2021-02-01 09:28:34 -0500

    done 5

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,30 @@
 const initialState = {};
 
 export default function cartReducer(state = initialState, action) {
+  const addedCart = {...state}
+  console.log(addedCart)
   switch (action.type) {
     case "ADD_ITEM": {
-      return {
-        ...state,
-        [action.item.id]: {
-          ...action.item,
-          quantity: 1,
-        },
-      };
+      if (addedCart[action.item.id]) {
+        const quantity = addedCart[action.item.id].quantity
+        console.log(quantity)
+        return {
+          ...state,
+          [action.item.id]: {
+            ...action.item,
+            quantity: quantity + 1
+          }
+        }
+      }
+      else {
+        return {
+          ...state,
+          [action.item.id]: {
+            ...action.item,
+            quantity: 1,
+          },
+        };
+      }
     }
     case "REMOVE_ITEM": {
       const newCart = { ...state };
